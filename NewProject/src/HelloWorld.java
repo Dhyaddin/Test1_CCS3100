a9687ddda2b79fe9eb2d503f6e6748488370dbba
MUHAMMAD DHIYAUDDIN <odendin8@gmail.com>
2024-10-16 11:33:32 +0800

    Third Commit

--- NewProject/src/HelloWorld.java
+++ NewProject/src/HelloWorld.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		Scanner rd = new Scanner(System.in);
+		/*Scanner rd = new Scanner(System.in);
 		System.out.println("Enter the string you want show :");
 		String a = rd.nextLine();
 		System.out.println("Your String Output : " + a);
-		rd.close();
+		rd.close();*/
+		System.out.println("Hello World UPM !");
 	}
 }
---
+++ NewProject/src/MakingMoneyATM.java
@@ -0,0 +1,12 @@
+
+public class MakingMoneyATM {
+	public static void main (String args[]) {
+		System.out.println("********************************************************************");
+		System.out.println("*" + "\t\t\t\t\t\t\t\t   *");
+		System.out.println("*" + "\t\t Making Money Bank ATM Service \t\t\t" + "   *");
+		System.out.println("*" + "\t\t\t\t\t\t\t\t   *");
+		System.out.println("********************************************************************");
+		System.out.println("\n Welcome !");
+	}
+
+}
